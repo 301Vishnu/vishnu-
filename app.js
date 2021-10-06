fefeb9b9831bf6370422cd6f5d2ac13e2c3d0098
301Vishnu <83953755+301Vishnu@users.noreply.github.com>
2021-10-06 19:07:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const play = document.getElementById("button")
+
+play.onclick = () => {
+location.href = "./thanks.html"
+
+}
+var secondParagraph = document.getElementById( "yes" );
+
+document.getElementById( "secondpara" ).onclick = () =>  {
+document.getElementById( "secondpara" ).innerHTML = "thank u for your reply";}
+
+
